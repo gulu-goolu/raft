5b3a7f5d571ea35b388789caea3d1df8e0637196
murmur.wheel <murmur.wheel@gmail.com>
2019-06-04 01:48:11 +0800

    update

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,59 @@
+use std::cell::RefCell;
+use std::env;
+use std::net::{TcpListener, TcpStream, Ipv4Addr};
+
+const USAGE: &'static str = "
+Usage:
+  node '{}'
+";
+
+/// 启动参数
+/// ```json
+/// {
+///     "bind": {
+///         "ip": "127.0.0.1",
+///         "port": 8889
+///     },
+///     "peers":[{
+///         "ip": "12.17.0.2",
+///         "port": 9900
+///     }]
+/// }
+/// ```
+struct Addr {
+    ip: String,
+    port: u32,
+}
+struct Node {
+    bind: Addr,
+    peers: Vec<Addr>,
+}
+
+impl Node {
+    ///
+    /// 从命令行初始化节点
+    fn from_args() -> Node {
+        for argument in env::args() {
+            println!("args = {}", argument);
+        }
+        // 初始化节点
+        Node {
+            bind: {
+                ip: String::from("123"),
+                port: 0,
+            },
+            peers: vec![],
+        }
+    }
+
+    /// 运行此节点
+    fn run(&self) {
+        println!("run");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", args);
+    let node = Node::from_args();
+    node.run();
 }
